e7f446415147f74286698aa6f7f5edd43c65d027
lucas <31916378+smokals@users.noreply.github.com>
2018-03-28 21:06:31 +1000

    tabs > spaces

--- bot.js
+++ bot.js
@@ -23,63 +23,63 @@ aboutEmbed.addField("Report A Bug:", "[GitHub Issues Page](https://github.com/sm
 
 client.on("ready", function()
 {
-  console.log("--> bot ready!\n-> logged in with token: " + token + "\n-> user id: " + client.user.id);
-  console.log("\n\nWaiting for commands...");
+	console.log("--> bot ready!\n-> logged in with token: " + token + "\n-> user id: " + client.user.id);
+	console.log("\n\nWaiting for commands...");
 });
 
 client.on("disconnected", function()
 {
-   console.log("\nDisconnected!"); // from a bot i made a long time ago, might not work (or even have a purpose)
-   process.exit(1);
+	console.log("\nDisconnected!");
+	process.exit(1);
 });
 
 client.on("message", function(message) // calls every time a message is sent
 {
-  if (!message.author.id == client.user.id) return;
-  if (!message.content.startsWith(prefix)) return;
+	if (!message.author.id == client.user.id) return;
+	if (!message.content.startsWith(prefix)) return;
 
   var args = message.content.substring(prefix.length).split(" ");
 
   switch (args[0].toLowerCase())
   {
-    case "ping":
-      message.channel.send("Pong!");
-      message.channel.send(new Date().getTime() - message.createdTimestamp + " ms"); // this sends a negative ping for some bizare reason...
-      break;
+	case "ping":
+		message.channel.send("Pong!");
+		message.channel.send(new Date().getTime() - message.createdTimestamp + " ms"); // this sends a negative ping for some bizare reason...
+		break;
 
-   case "embed": // command format: ;embed [TITLE] [COLOUR_IN_HEX] [Body Text]
-      embed = new Discord.RichEmbed();
-      embed.addField(args[1], args.slice(3).join(" "));
-      embed.setColor(args[2]);
-      message.channel.send(embed);
-      break;
+	case "embed": // command format: ;embed [TITLE] [COLOUR_IN_HEX] [Body Text]
+	  embed = new Discord.RichEmbed();
+	  embed.addField(args[1], args.slice(3).join(" "));
+	  embed.setColor(args[2]);
+	  message.channel.send(embed);
+	  break;
 
-   case "about":
-      message.channel.send(aboutEmbed);
-      break;
+	case "about":
+	  message.channel.send(aboutEmbed);
+	  break;
 
-   case "kick": // command format: ;embed [@mention_user] [reason for kick]
-      message.mentions.members.first().kick(args.slice(2).join(" ")).then((member) =>
-      {   // success
-         message.channel.send(":wave: " + message.mentions.members.first().displayName + " has been kicked for `" + args.slice(2).join(" ") + "`");
-      }).catch(() =>
-      {   // error
-         message.channel.send("`The mentioned member could not be kicked!`");
-      });
-      break;
+	case "kick": // command format: ;embed [@mention_user] [reason for kick]
+		message.mentions.members.first().kick(args.slice(2).join(" ")).then((member) =>
+		{   // success
+			message.channel.send(":wave: " + message.mentions.members.first().displayName + " has been kicked for `" + args.slice(2).join(" ") + "`");
+		}).catch(() =>
+		{   // error
+			message.channel.send("`The mentioned member could not be kicked!`");
+		});
+		break;
 
-   case "ban": // command format: ;embed [@mention_user] [reason for ban]
-      message.mentions.members.first().ban(args.slice(2).join(" ")).then((member) =>
-      {   // success
-         message.channel.send(":wave: " + message.mentions.members.first().displayName + " has been banned for `" + args.slice(2).join(" ") + "`");
-      }).catch(() =>
-      {   // error
-         message.channel.send("`The mentioned member could not be banned!`");
-      });
-      break;
+	case "ban": // command format: ;embed [@mention_user] [reason for ban]
+		message.mentions.members.first().ban(args.slice(2).join(" ")).then((member) =>
+		{   // success
+			message.channel.send(":wave: " + message.mentions.members.first().displayName + " has been banned for `" + args.slice(2).join(" ") + "`");
+		}).catch(() =>
+		{   // error
+			message.channel.send("`The mentioned member could not be banned!`");
+		});
+		break;
 
-    default:
-      message.channel.send("`Invalid command!`");
+	default:
+		message.channel.send("`Invalid command!`");
   }
 });
 
